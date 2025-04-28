ed5d71151a4c7a4a9e908c180ed21ec628fafb4b
TheLegendaryBlade <coltono@rocketmail.com>
2025-04-28 15:42:56 -0700

    Update Software.java

--- Software.java
+++ Software.java
@@ -5,22 +5,24 @@ public class Software {
     
     //Private Variables
     private String id;  //Identification for software
-	private String name;    //Name of software
-	private String version; //Version number of software
+    private String name;    //Name of software
+    private String version; //Version number of software
     private String releaseDate; //Release date of software
+    private String softwareType; //Type of software
 	
 	//Default constructor
     public Software() {
-	    this.id = "N/A";
-	    this.name = "N/A";
-	    this.version = "N/A";
+	this.id = "N/A";
+	this.name = "N/A";
+	this.version = "N/A";
         this.releaseDate = "N/A";
 	}
-	public Software(String id, String name, String version, String releaseDate) {
-	    this.id = id;
-	    this.name = name;
-	    this.version = version;
+	public Software(String id, String name, String version, String releaseDate, String softwareType) {
+	this.id = id;
+	this.name = name;
+	this.version = version;
         this.releaseDate = releaseDate;
+	this.softwareType = softwareType;
 	}
 	
 	//Getters
@@ -39,6 +41,10 @@ public class Software {
     public String getReleaseDate() {
     return releaseDate;
     }
+
+    public String getSoftwareType() {
+    return softwareType;
+    }
 	
 	//Setters
     public void setID(String id) {
@@ -55,5 +61,9 @@ public class Software {
 
     public void setReleaseDate(String releaseDate) {
     this.releaseDate = releaseDate;
+    }
+
+    public void setSoftwareType(String softwareType) {
+    this.softwareType = softwareType;
     }
 }
